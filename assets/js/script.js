74f83d675e3146ffe5d59f71aa1097b802863ec6
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-14 23:48:36 +0200

    da completare

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ async function chargeApi() {
 
   for (let index = 0; index < albumTot.length; index++) {
     document.querySelector("#sectionAlbum").innerHTML += `<div class="card" style="width: 15rem;">
-        <img class="imgCover" src= "${albumTot[index].album.cover_medium}" class="card-img-top" alt="img">
-        <div class="card-body">
+    <div class="card-body">
+    <img class="imgCover" src= "${albumTot[index].album.cover_medium}" class="card-img-top" alt="img">
           <h5 class="card-title">${albumTot[index].artist.name}</h5>
           <p class="card-text">${albumTot[index].album.title}</p>
             <audio controls>
@@ -25,11 +25,25 @@ async function chargeApi() {
       </div>`;
   }
 
-  const searchAlbum = (query) => {
-    return Object.keys(albumTot).filter((item) => {
-      return item.album.title.includes(query);
-    });
-  };
-}
+  // const searchAlbum = (query) => {
+  //   return Object.keys(albumTot).filter((item) => {
+  //     return item.album.title.includes(query);
+  //   });
+  // };
 
+  function searchAlbum() {
+    let input = document.querySelector("#searchbar").value;
+    let filter = input.toUpperCase();
+    let cards = document.querySelectorAll("#sectionAlbum");
+    for (i = 0; i < cards.length; i++) {
+      txtValue = [i].textContent || cards[i].innerHTML;
+      if (txtValue.toUpperCase().index0f(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
+  console.log(searchAlbum());
+}
 chargeApi();
