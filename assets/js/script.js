34a42acfbc4f4331c3d988d6620864d534b40cf0
Nicola Lerra <github.nicolalerra@gmail.com>
2022-10-15 18:42:56 +0200

    completato

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ async function chargeApi() {
   let linkArtist = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=Nicola%20Lerra`);
   let responseText = await linkArtist.json();
   let musicArray = responseText.data;
-  console.log(musicArray);
 
   const albumOne = musicArray.slice(0, 4);
   const albumTwo = musicArray.slice(6, 9);
@@ -10,8 +9,6 @@ async function chargeApi() {
   const albumFour = musicArray.slice(16, 18);
 
   let albumTot = albumOne.concat(albumTwo, albumThree, albumFour);
-  console.log(albumTot);
-
   for (let index = 0; index < albumTot.length; index++) {
     document.querySelector("#sectionAlbum").innerHTML += `<div class="card" style="width: 15rem;">
     <div class="card-body">
@@ -25,25 +22,40 @@ async function chargeApi() {
       </div>`;
   }
 
-  // const searchAlbum = (query) => {
-  //   return Object.keys(albumTot).filter((item) => {
-  //     return item.album.title.includes(query);
-  //   });
-  // };
-
-  function searchAlbum() {
-    let input = document.querySelector("#searchbar").value;
-    let filter = input.toUpperCase();
-    let cards = document.querySelectorAll("#sectionAlbum");
-    for (i = 0; i < cards.length; i++) {
-      txtValue = [i].textContent || cards[i].innerHTML;
-      if (txtValue.toUpperCase().index0f(filter) > -1) {
-        cards[i].style.display = "";
-      } else {
-        cards[i].style.display = "none";
+  const searchBar = document.getElementById("searchBar");
+  searchBar.addEventListener("keyup", (e) => {
+    document.querySelector("#sectionAlbum").innerHTML = "";
+    const searchString = e.target.value;
+    albumTot.filter((song) => {
+      filter = song.title.toLowerCase().includes(searchString.toLowerCase());
+      if (filter) {
+        document.querySelector("#sectionAlbum").innerHTML += `<div class="card" style="width: 15rem;">
+                                                                <div class="card-body">
+                                                                <img class="imgCover" src= "${song.album.cover_medium}" class="card-img-top" alt="img">
+                                                                      <h5 class="card-title">${song.artist.name}</h5>
+                                                                      <p class="card-text">${song.album.title}</p>
+                                                                        <audio controls>
+                                                                            <source src=${song.preview} type="audio/ogg"> </div> 
+                                                                        </audio>
+                                                                    </div>
+                                                                  </div>`;
       }
-    }
-  }
-  console.log(searchAlbum());
+    });
+  });
 }
 chargeApi();
+
+// function searchAlbum() {
+//   let input = document.querySelector("#searchbar").value;
+//   let filter = input.toUpperCase();
+//   let cards = document.querySelectorAll("#sectionAlbum");
+//   for (i = 0; i < cards.length; i++) {
+//     txtValue = [i].textContent || cards[i].innerHTML;
+//     if (txtValue.toUpperCase().index0f(filter) > -1) {
+//       cards[i].style.display = "";
+//     } else {
+//       cards[i].style.display = "none";
+//     }
+//   }
+// }
+// console.log(searchAlbum());
